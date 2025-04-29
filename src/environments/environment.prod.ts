ab4f1e5edac6d17adb689e840aa6f7d0849f7e52
AntonPo <myworkinggemail@gmail.com>
2025-04-29 19:16:42 +0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dotnet-postgresql-service-864171160719.us-central1.run.app/'
+  //baseUrl: 'https://dotnet-postgresql-service-864171160719.us-central1.run.app/'
+  baseUrl: 'https://anton-posgres-app.azurewebsites.net/'
 };
